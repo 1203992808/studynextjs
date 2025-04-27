61f14f5828020cd3f150bd1c09c763b06bc22de3
1203992808@qq.com <1203992808@qq.com>
2025-04-27 08:28:07 +0800

    usePathname学习

--- app/databoard/layout.tsx
+++ app/databoard/layout.tsx
@@ -1,7 +1,19 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { useState } from "react";
+
+const linkData=[
+  {
+    name:"Abount",
+    path:"/databoard/about"
+  },
+  {
+    name:"Setting",
+    path:"/databoard/setting"
+  },
+]
 
 export default function DataBoardLayout({ 
   children 
@@ -9,14 +21,21 @@ export default function DataBoardLayout({
   children: React.ReactNode 
 }) {
 const[count,setCount]=useState(0);
+const pathname=usePathname();
+console.log(pathname);
 
 
   return (
     <div className="border-2 border-dashed border-black p-4 
     w-1/2 mt-10 ">
-        <div className="flex gap-4 font-bold text-lg mb-4 text-purple-500">
-        <Link href="/databoard/about">Abount</Link>
-        <Link href="/databoard/setting">Setting</Link>
+        <div className="flex gap-4 font-bold text-lg mb-4">
+            {linkData.map((link)=>(
+                <Link key={link.path} className={pathname===link.path?"text-purple-500":"text-black"} href={link.path}>{link.name}</Link>
+            ))}
+
+        {/* <Link className={pathname==="/databoard/about"?"text-purple-500":"text-black"} href="/databoard/about">Abount</Link>
+        <Link className={pathname==="/databoard/setting"?"text-purple-500":"text-black"} href="/databoard/setting">Setting</Link> */
+        }
       </div>
       <h2>DataBoardLayout {count}</h2>
 
